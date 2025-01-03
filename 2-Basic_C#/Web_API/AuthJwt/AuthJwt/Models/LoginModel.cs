8cfc46f6f17a5fc8081a6ea91925eda5c5ab06ee
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-01-03 17:14:40 +0530

    add JwtTokenBasedAuthentication

---
+++ 2-Basic_C#/Web_API/AuthJwt/AuthJwt/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthJwt.Models
+{
+    public class LoginModel
+    {
+        // The username provided by the user during login
+        public string Username { get; set; }
+
+        // The password provided by the user during login
+        public string Password { get; set; }
+    }
+}
