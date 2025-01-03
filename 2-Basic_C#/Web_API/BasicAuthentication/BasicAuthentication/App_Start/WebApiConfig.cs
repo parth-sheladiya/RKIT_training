f12fb22ef4caf21650b1bd7532b9ef59a2d9032e
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-01-03 11:34:10 +0530

    basic auth

---
+++ 2-Basic_C#/Web_API/BasicAuthentication/BasicAuthentication/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using BasicAuthentication.auth;
+
+namespace BasicAuthentication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Adding the BasicAuthentication filter to the global filter collection
+            config.Filters.Add(new BasicAuthenticationAttr());
+            // Web API configuration and services
+
+
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ 2-Basic_C#/Web_API/BasicAuthentication/BasicAuthentication/Controllers/BasicAuthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BasicAuthentication.auth;
+
+namespace BasicAuthentication.Controllers
+{
+    public class BasicAuthController : ApiController
+    {
+        [BasicAuthenticationAttr]
+        public string Get()
+        {
+            return "web api method call";
+        }
+    }
+}
