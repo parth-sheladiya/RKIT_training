761d95f7cb3e6117ef12208238874fa8c57dd120
parth sheladiya <parthsheladiya1427@gmail.com>
2025-01-06 22:13:43 +0530

    changes

---
+++ 2-Basic_C#/3-Web_API/BasicAuthDemo/BasicAuthDemo/Handlers/BasicAuthHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web.Http.Filters;
+
+namespace BasicAuthDemo.Handlers
+{
+    public class BasicAuthHandler : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            // Check if Authorization header exists
+            var authHeader = actionContext.Request.Headers.Authorization;
+            if (authHeader != null && authHeader.Scheme == "Basic")
+            {
+                var encodedCredentials = authHeader.Parameter;
+                var decodedBytes = Convert.FromBase64String(encodedCredentials);
+                var decodedCredentials = Encoding.UTF8.GetString(decodedBytes);
+                var credentials = decodedCredentials.Split(':');
+
+                var username = credentials[0];
+                var password = credentials[1];
+
+                // Validate credentials (hardcoded for demo)
+                if (username == "admin" && password == "password")
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                    return;
+                }
+            }
+
+            // If invalid, return 401 Unauthorized
+            actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized)
+            {
+                Content = new System.Net.Http.StringContent("Unauthorized")
+            };
+        }
+    }
+}
--- 2-Basic_C#/Web_API/BasicToDoApi/BasicToDoApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace BasicToDoApi.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
