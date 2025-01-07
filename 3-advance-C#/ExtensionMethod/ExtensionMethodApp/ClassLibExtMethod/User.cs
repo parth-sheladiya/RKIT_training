b714fc55232a6abe1e4c482770d84d9180f24067
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-01-07 16:31:50 +0530

    add  extension method
    class library use in this topic

---
+++ 3-advance-C#/ExtensionMethod/ExtensionMethodApp/ClassLibExtMethod/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibExtMethod
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+    }
+    /// <summary>
+    /// extension method 
+    /// it's must be in static class 
+    /// it's not modified.
+    /// 2 extension method
+    /// multiple extension method can be allow in static class 
+    /// </summary>
+    public static class UserExtensions
+    {
+        // Extension method
+        public static bool Login(this User user, string inputUsername, string inputPassword)
+        {
+            return user.Username == inputUsername && user.Password == inputPassword;
+        }
+
+        // Extension method to check user status
+        public static void CheckStatus(this User user)
+        {
+            // using string interpolation and ternary operatoe
+            Console.WriteLine(user.IsActive
+                ? $"User {user.Username} is active."
+                : $"User {user.Username} is inactive.");
+        }
+    }
+}
---
+++ 3-advance-C#/ExtensionMethod/ExtensionMethodApp/ExtensionMethodApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+// import class library in same assembly
+using ClassLibExtMethod;
+
+namespace ExtensionMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new User object
+            User user = new User { Username = "admin", Password = "1234", IsActive = true };
+
+            // Take user input for username and password
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            // Check login credentials using the extension method
+            if (user.Login(username, password))
+            {
+                Console.WriteLine("Login Successful!");
+                // useer is active or not 
+                user.CheckStatus(); 
+            }
+            else
+            {
+                Console.WriteLine("Login Failed!");
+            }
+        }
+    }
+}
