c503f055031e13fc65e8e7722845b5d71d01ad7c
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-01-09 18:41:20 +0530

    add LINQ

---
+++ 3-advance-C#/LinQExample/LinQExample/ConversionOperatorClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQExample
+{
+    public class ConversionOperatorClass
+    {
+        public static void RunConversionOperatorClass()
+        {
+            Conversion conversion = new Conversion();
+           
+            // to list method 
+            List<string> resForLst = conversion.countries.ToList();
+
+
+            Console.WriteLine("string convert to tolist");
+
+            foreach(string s in resForLst)
+            {
+                Console.WriteLine(s);
+            }
+
+            // to array method 
+            string[] countryArray = conversion.countries.ToArray();
+
+            Console.WriteLine("string convert to str array");
+
+            foreach(string i in countryArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            // as enumerable method 
+
+            Console.WriteLine("as enumerable method is ");
+
+            var resForEnumMethod = conversion.countries.AsEnumerable();
+
+            foreach(string i in resForEnumMethod)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        public class Conversion
+        {
+            // data source 
+           public string[] countries = { "US", "UK", "India", "Russia", "China", "Australia", "Argentina" };
+
+
+        }
+    }
+}
---
+++ 3-advance-C#/LinQExample/LinQExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinQExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //call method to query sytax class 
+            /// QuerySyntax.RunQuerySyntax();
+
+
+            // call method to mwthod class systax 
+            /// MethodSyntaxClass.RunMethodSyntaxClass();
+
+            // call method to aggreate function 
+            //AggregateFunctionClass.RunAggregateFunctionClass(); 
+
+            // call method to sorting function
+            //SortingOperatorClass.RunSortingOperatorClass();
+
+            // call method to partition class
+            // PartitionOperatorClass.RunPartitionOperatorClass();
+
+            // call nmethod to convertion class 
+            ConversionOperatorClass.RunConversionOperatorClass();
+        }
+    }
+}
