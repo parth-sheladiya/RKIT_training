ec250eacfc382d37a5645b7ca94b630ebc7f0a84
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-01-24 12:29:32 +0530

    add custom middleware

--- 4-Asp.net-Core/CustomMiddleware/CustomMiddleware/RequestLoggingMiddleware.cs
+++ 4-Asp.net-Core/CustomMiddleware/CustomMiddleware/RequestLoggingMiddleware.cs
@@ -7,18 +7,31 @@ namespace CustomMiddleware
 {
     #region RequestLoggingMiddleware Class
     // This class represents the custom middleware that logs HTTP request details
+    /// <summary>
+    /// Middleware that logs HTTP request details including method, URL, and timestamp.
+    /// This middleware allows you to monitor and log the requests passing through the pipeline.
+    /// </summary>
     public class RequestLoggingMiddleware
     {
-        // it is use to process http request and invokle next request
+        // it is used to process HTTP request and invoke the next request in the pipeline
         private readonly RequestDelegate _next;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestLoggingMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The next middleware delegate in the pipeline.</param>
         public RequestLoggingMiddleware(RequestDelegate next)
         {
             _next = next;
         }
 
-        // it is entry point of custom middleware
-        // it is method to process in midleware pipeline
+        // it is the entry point of the custom middleware
+        // this method processes the HTTP request in the middleware pipeline
+        /// <summary>
+        /// Logs the HTTP request details (method, URL, timestamp) and passes control to the next middleware.
+        /// </summary>
+        /// <param name="context">The HTTP context for the current request.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public async Task InvokeAsync(HttpContext context)
         {
             // Log the request details (method, URL, and timestamp)
@@ -31,18 +44,6 @@ namespace CustomMiddleware
 
             // Continue to the next middleware in the pipeline
             await _next(context);
-        }
-    }
-    #endregion
-
-    #region RequestLoggingMiddlewareExtensions Class
-    // This class provides an extension method to add the RequestLoggingMiddleware to the pipeline
-    public static class RequestLoggingMiddlewareExtensions
-    {
-        public static IApplicationBuilder UseRequestLoggingMiddleware(this IApplicationBuilder builder)
-        {
-            // Add the RequestLoggingMiddleware to the HTTP request pipeline
-            return builder.UseMiddleware<RequestLoggingMiddleware>();
         }
     }
     #endregion
