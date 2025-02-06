363241f3e6a800776d3d2f3625bef36aa8248bd3
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-02-06 13:32:03 +0530

    add code

--- 5-DevExtreme/DevExtreme/EditorOverview/DropDown/dropDown.js
+++ 5-DevExtreme/DevExtreme/EditorOverview/DropDown/dropDown.js
@@ -10,27 +10,60 @@
         displayExpr: "name",
         height: "100px",
         width: "500px",
-        showDropDownButton:true
+        showDropDownButton: true,
+
     })
 
-
-
-
-
-
-
-
-    var names = ["parth", "raj", "deep", "jay", "jeel"]
+    var names = ["parth", "raj", "deep", "jay", "jeel"];
 
     $("#dropDownBox").dxDropDownBox({
         contentTemplate: function (e) {
             var $list = $("<div>").dxList({
-                dataSource:names
-            })
+                dataSource: names,
+                selectionMode: "multiple", // single
+                showSelectionControls: true,
+                deferRendering: true
+            });
             return $list;
-            
-           
         },
-        displayExpr: "name",
-    })
+
+        onClosed: function () {
+            alert("dropdown close");
+        },
+        //buttons: [
+        //    {
+        //        name: "clear",
+        //        location: "after",
+        //        options: {
+        //            text: "❌",
+        //            onClick: function () {
+        //                console.log("click call");
+        //            }
+        //        },
+        //    }
+        //],
+
+        hoverStateEnabled: true,
+        showClearButton: true, // Make sure this is set to true
+        showDropDownButton: true // Also, enable the dropdown button explicitly
+    });
+
+
+    var dropDownInstance = $("#dropDownBox").dxDropDownBox("instance")
+    dropDownInstance.on("optionChanged", function (e) {
+        console.log(`Option Changed: ${e.name} => ${e.value}`);
+    });
+
+    dropDownInstance.on("focus", function () {
+        console.log("Dropdown focused");
+    });
+
+    dropDownInstance.on("blur", function () {
+        console.log("Dropdown lost focus");
+    });
+
+    dropDownInstance.on("keydown", function (e) {
+        console.log(`Key pressed: ${e.event.key}`);
+    });
+    
 });
---
+++ 5-DevExtreme/DevExtreme/EditorOverview/NumberBox/NumberBox.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    $("#numberBox").dxNumberBox({
+        placeholder:"this is number box",
+        // short cut key 
+        accessKey: "j",
+        //when the widget is focused or clicked
+        activeStateEnabled: true,
+        // bydefault false
+        // if true then disabled
+        disabled: false,  
+        elementAttr: {
+            //customid
+            id: "id-num-box",
+            //customclass
+            class:"class-num-box"
+        },
+        hint: "this is number box",
+        max: 1000,
+        min: 5,
+        mode: "number", // default teext , number , // number , txt,tel
+        readOnly: false,
+        showClearButton: true,
+        showSpinButtons: true,
+        step: 20,
+        visible: true,
+        useLargeSpinButtons:true,
+        value:"5",
+
+
+
+    })
+})
