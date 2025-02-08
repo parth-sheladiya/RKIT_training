984f8b77f8ebb1c16f5f5aad6d6bf0ed7919de9d
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-02-08 16:09:43 +0530

    add code

---
+++ 5-DevExtreme/DevExtreme/EditorOverview/ButtonBox/Button.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function (){
+    $("#ButtonBox").dxButton({
+        text: "click me",
+        type:"normal",
+        onClick: function () {
+            DevExpress.ui.notify("button clicked")
+        },
+        stylingMode: "outlined",
+        icon: "activefolder" // search deveextreme icons  different icons are available in devextreme 
+
+    })
+
+    $("#successButtonBox").dxButton({
+        text: "sucess",
+        type: "success",
+        stylingMode: "text", // outlined contained
+        icon: "comment",
+        onClick: function () {
+            DevExpress.ui.notify("success button clicked ","success",6000)
+        }
+    })
+
+    $("#defaultButtonBox").dxButton({
+        text: "default button",
+        type: "default",
+        stylingMode: "contained", //outlined text
+        onClick: function () {
+            DevExpress.ui.notify("default button clicked","info",4000)
+        },
+        icon:"airplane"
+    })
+
+    $("#DengerButton").dxButton({
+        text: "danger button",
+        type: "danger",
+        stylingMode: "contained",
+        onClick: function () {
+            DevExpress.ui.notify("danger button clicked","warning",1000)
+        },
+        icon:"comment"
+    })
+})
