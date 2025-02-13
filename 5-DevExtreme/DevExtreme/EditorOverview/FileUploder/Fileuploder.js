672359083aa00a20046845a2aebbae361cb0426a
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-02-13 12:00:32 +0530

    drag drop file

--- 5-DevExtreme/DevExtreme/EditorOverview/FileUploder/Fileuploder.js
+++ 5-DevExtreme/DevExtreme/EditorOverview/FileUploder/Fileuploder.js
@@ -2,15 +2,16 @@
     $("#fileUploder").dxFileUploader({
         // abort process cancels the file upload
         abortUpload: () => { },
-        accept: "image/*",
-        accessKey: "h", // accept only image files
-        allowCanceling:true, // user can cancle file upload default false
+        accept: "image/*",// accept only image files
+        accessKey: "h", 
+        allowCanceling:true, // user can cancle file upload default true
         allowedFileExtensions: [".jpg", ".jpeg"] , // default value empty arr of str
         hint: "this is file content",
         selectButtonText:"upload file",  // btn text
         invalidFileExtensionMessage: "this extension is invalid",
         invalidMaxFileSizeMessage: "your file size is greater than maxsize",
         invalidMinFileSizeMessage: "your file size is less than min size",
+        uploadFailedMessage:"upload failed ",
         labelText: "file upload here",
         maxFileSize: 1048576,
         minFileSize: 1024,
@@ -20,8 +21,20 @@
             console.log("Upload progress:", e.bytesLoaded / e.bytesTotal * 100, "%");
         },
         readyToUploadMessage: "file is ready to upload", 
-        
+        uploadMethod:"POST",
+        uploadMode: "useButtons",
+
     })
 
-    
+    $("#fileUploader").dxFileUploader({
+        uploadUrl: "/upload", 
+        multiple: true,
+        uploadMode: "useButtons",
+        dropZone: "#drop-area"
+    });
+
 })
+
+
+
+    
