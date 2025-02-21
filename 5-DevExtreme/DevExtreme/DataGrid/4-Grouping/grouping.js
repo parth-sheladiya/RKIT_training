5631a21fa64a60ca5b28f3131883e23584e90cd8
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-02-21 15:23:33 +0530

    add grouping code

--- 5-DevExtreme/DevExtreme/DataGrid/4-Grouping/grouping.js
+++ 5-DevExtreme/DevExtreme/DataGrid/4-Grouping/grouping.js
@@ -4,19 +4,22 @@ $(document).ready(function(){
     $("#GroupContainer").dxDataGrid({
         dataSource:products,
         showBorders:true,
-       
+        customizeColumns: (columns) => {
+          columns[0].width = 100;
+        
+        },
         // user can rearrange column
         allowColumnReordering:false,
         columns:[
           {  
             dataField:"productId",
             dataType:"number",
-            allowGrouping: false,
+          //  allowGrouping: false,
           },
           {  
             dataField:"productName",
             dataType:"string",
-            allowGrouping: false,
+           // allowGrouping: false,
           },
           {  
             dataField:"productCategory",
@@ -34,11 +37,25 @@ $(document).ready(function(){
           },
         grouping:{
             // expand data
-            autoExpandAll: false,
+            autoExpandAll: true,
             // user can not expand data only show category
-            allowCollapsing:false,
-            expandMode: "buttonClick", //  buttonclick rowclick
+            allowCollapsing:true,
+            expandMode: "rowClick", //  buttonclick rowclick
+            // user can right click then show group by this column
+            contextMenuEnabled:true,
+            texts:{
+              // default string  check docs
+              groupByThisColumn:"please grouping",
+              groupContinuedMessage:"hello",
+              ungroup:"ungroup please",
+              ungroupAll:"please ungroup all"             
+            },
+        },
+        groupPanel:{
+       // show on ui
+          visible:true,
+          allowColumnDragging:true,
+          emptyPanelText:"Drag a column header here to group by that column"
         }
     })
-
 })
