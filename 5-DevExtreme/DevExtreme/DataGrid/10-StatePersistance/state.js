77b21e4dc2fc5cf761d6dcbf15a0dbadd2c3775a
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-02-25 12:31:41 +0530

    add code

---
+++ 5-DevExtreme/DevExtreme/DataGrid/10-StatePersistance/state.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+    console.log("docs is ready");
+
+    var dataGrid = $("#StateContainer").dxDataGrid({
+        dataSource: products,
+        showBorders: true,
+        allowColumnReordering: true,
+        allowColumnResizing: true,
+        rowAlternationEnabled: true,
+        selection: {
+            mode: 'single',
+        },
+        filterRow: {
+            visible: true,
+        },
+        groupPanel: {
+            visible: true,
+        },
+        stateStoring: {
+            enabled: true,
+            type: "localStorage", // 'custom' | 'localStorage' | 'sessionStorage'
+            storageKey: "statepersistanceKey",
+            savingTimeout: 3000
+        },
+        columns: [
+            {
+                dataField: "productId",
+                dataType: "number",
+                alignment: "right"
+            },
+            {
+                dataField: "productName",
+                dataType: "string",
+                alignment: "right"
+            },
+            {
+                dataField: "productCategory",
+                dataType: "string",
+                alignment: "right"
+            },
+            {
+                dataField: "productPrice",
+                dataType: "number",
+                alignment: "right"
+            },
+            {
+                dataField: "productRating",
+                dataType: "number",
+                alignment: "right"
+            },
+            {
+                dataField: "productStock",
+                dataType: "number",
+                alignment: "center"
+            }
+        ],
+    }).dxDataGrid("instance");
+
+    // Save the grid state to 
+    $("#saveState").dxButton({
+        text: "save state",
+        onClick: function(){
+            const stateData = dataGrid.state();
+            localStorage.setItem("dataGridState", JSON.stringify(stateData)); 
+            DevExpress.ui.notify("State Saved", "success", 2000);
+        }
+    });
+
+    // Load the grid state from 
+    $("#loadState").dxButton({
+        text: "load state",
+        onClick: function(){
+            const stateData = localStorage.getItem("dataGridState");
+            if (stateData) {
+                dataGrid.state(JSON.parse(stateData));
+                DevExpress.ui.notify("State Loaded", "default", 2000);
+            } else {
+                DevExpress.ui.notify("No saved state found", "error", 2000);
+            }
+        }
+    });
+});
