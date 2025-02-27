57dba396a1565935ebfe270f3af453913fdbece7
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-02-27 10:22:07 +0530

    update toolbar

--- 5-DevExtreme/DevExtreme/DataGrid/12-Template/ToolBar.js
+++ 5-DevExtreme/DevExtreme/DataGrid/12-Template/ToolBar.js
@@ -1,63 +1,115 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("docs is ready");
 
-      $("#ToolbarContainer").dxDataGrid({
-        dataSource:orders,
-        keyExpr:"ID",
-        showBorders:true,
+    var gridInstance = $("#ToolbarContainer").dxDataGrid({
+        dataSource: orders,
+        keyExpr: "ID",
+        showBorders: true,
         showColumnLines: true,
         showRowLines: true,
         rowAlternationEnabled: true,
-        columnAutoWidth:true,
-        loadPanel:{
-            enabled:true
+        columnAutoWidth: true,
+        loadPanel: {
+            enabled: true
         },
-        columns:[
+        columns: [
             {
-                dataField:"OrderNumber",
-                dataType:"number",
-                caption:"Order number"
+                dataField: "OrderNumber",
+                dataType: "number",
+                caption: "Order number"
             },
             {
-                dataField:"OrderDate",
-               // dataType:"date",
-                caption:"Order date"
-            }, 
-            {
-                dataField:"Employee",
-                dataType:"string",
-                caption:"Employee Name"
-            }, 
-            {
-                dataField:"TotalAmount",
-                dataType:"number",
-                caption:"Total Amount"
+                dataField: "OrderDate",
+                // dataType:"date",
+                caption: "Order date"
             },
             {
-                dataField:"SaleAmount",
-                dataType:"number",
-                caption:"Sale Amount"
+                dataField: "Employee",
+                dataType: "string",
+                caption: "Employee Name"
+            },
+            {
+                dataField: "TotalAmount",
+                dataType: "number",
+                caption: "Total Amount"
+            },
+            {
+                dataField: "SaleAmount",
+                dataType: "number",
+                caption: "Sale Amount"
             }
         ],
-        // toolbar:{
-        //     items:[
-        //        {
-        //         location:"before",
-        //         template:()=>{
-        //             return $('<div>')
-        //             .addClass('informer')
-        //             .append(
-        //               $('<div>')
-        //                 .addClass('count')
-        //                 .text(getGroupCount('CustomerStoreState')),
-        //               $('<span>')
-        //                 .text('Total Count'),
-        //             );
-        //         }
-        //        },
-        //     ]
-        // }
-        
-      })  
+        // filterPanel: {
+        //     visible: true,
+        // },
+        onToolbarPreparing: function (e) {
+            console.log("e is", e);
+            console.log("e.component is ", e.component);
+            console.log("component name is ", e.component.NAME)
+            var toolItems = e.toolbarOptions.items;
+            var dataGrid = e.component;
+            console.log(toolItems);
+
+            toolItems.push(
+                {
+                    widget: "dxButton",
+                    location: "after",
+                    options:{
+                        text:"order 2013",
+                        onClick: function () {
+                            // Find the latest order based on the OrderDate
+                            var orders2013 = orders.filter(function (order) {
+                                var orderDate = new Date(order.OrderDate);
+                                DevExpress.ui.notify("2013 order", "success", 500);
+                                return orderDate.getFullYear() === 2013;
+                                
+                            });
+    
+                            // Update the grid to show only the filtered orders
+                            dataGrid.option("dataSource", orders2013);
+    
+                        },
+                    },
+                    
+                }
+            );
+            toolItems.push(
+                {
+                    widget: "dxButton",
+                    options: {
+                        text: "popup",
+                        onClick: function () {
+                            gridInstance.option("editing.mode", "popup");
+                            DevExpress.ui.notify("success", "success", 500);
+                        },
+                    },
+                    location: "before",
+                }
+            );
+            toolItems.push(
+                {
+                    widget: "dxButton",
+                    options: {
+                        text: "Higest Total Amount",
+                        onClick: function () {
+                            var highestOrder = orders.reduce(function(prev, current) {
+                                DevExpress.ui.notify("highest total amount fetch successfully", "success", 500);
+                                return (prev.TotalAmount > current.TotalAmount) ? prev : current;
+                            });
+    
+                            // Update the grid to only show the order with the highest TotalAmount
+                            dataGrid.option("dataSource", [highestOrder]);
+                        },
+                    },
+                    //location: "before",
+                }
+            )
+
+
+        }
+
+
+
+    }).dxDataGrid("instance");
 
 })
