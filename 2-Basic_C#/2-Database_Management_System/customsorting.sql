644912fe6b1d3c227de5c80adf97f5f4dededb20
parth sheladiya <parthsheladiya1427@gmail.com>
2025-03-05 19:39:16 +0530

    custom sorting

---
+++ 2-Basic_C#/2-Database_Management_System/customsorting.sql
@@ -0,0 +1,34 @@
+-- 1 role sorting
+-- 2 e com store of my final demo asp .net core 
+-- order status Pending Processing Shipped  Delivered Cancelled
+
+CREATE DATABASE CompanyDB;
+USE CompanyDB;
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    role VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees (name, role, salary) VALUES
+('Amit', 'Manager', 80000),
+('Raj', 'Developer', 60000),
+('Suman', 'Intern', 20000),
+('Priya', 'Developer', 65000),
+('Vikas', 'Manager', 90000),
+('Neha', 'Intern', 25000),
+('Rohit', 'Developer', 62000),
+('Kunal', 'Manager', 85000),
+('Swati', 'Intern', 23000),
+('Pooja', 'Developer', 63000);
+
+
+
+SELECT * FROM employees;
+SELECT name,id,role,salary FROM employees;
+
+SELECT * FROM employees ORDER BY FIELD(role, 'Manager', 'Developer', 'Intern');
+SELECT * FROM employees ORDER BY FIELD(role, 'Manager', 'Developer', 'Intern'), salary DESC;
+
