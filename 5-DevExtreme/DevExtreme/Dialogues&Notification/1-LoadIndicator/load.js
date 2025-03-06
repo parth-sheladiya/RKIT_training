992b26f68663d232116c5410abd07548548fa08a
parth sheladiya <137270544+parth-sheladiya@users.noreply.github.com>
2025-03-06 14:38:56 +0530

    add code

---
+++ 5-DevExtreme/DevExtreme/Dialogues&Notification/1-LoadIndicator/load.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    console.log("doc is ready");
+
+    const cartsData = [
+        {
+            id: 1,
+            thumbnail: "https://cdn.dummyjson.com/products/images/vehicle/Charger%20SXT%20RWD/thumbnail.png"
+        },
+        {
+            id: 2,
+            thumbnail: "https://cdn.dummyjson.com/products/images/vehicle/Electric%20Scooter/thumbnail.png"
+        },
+        {
+            id: 3,
+            thumbnail: "https://cdn.dummyjson.com/products/images/vehicle/Charger%20SXT%20RWD/thumbnail.png"
+        },
+        {
+            id: 4,
+            thumbnail: "https://cdn.dummyjson.com/products/images/vehicle/Electric%20Scooter/thumbnail.png"
+        },
+        {
+            id: 5,
+            thumbnail: "https://cdn.dummyjson.com/products/images/vehicle/Charger%20SXT%20RWD/thumbnail.png"
+        },
+        {
+            id: 6,
+            thumbnail: "https://cdn.dummyjson.com/products/images/vehicle/Electric%20Scooter/thumbnail.png"
+        }
+    ];
+
+    $("#btnLoad").dxButton({
+        text: "send",
+        height: 50,
+        width: 180,
+        template(data, container) {
+            $(`<div class='button-indicator'></div><span class='dx-button-text'>${data.text}</span>`).appendTo(container);
+            buttonIndicator = container.find('.button-indicator').dxLoadIndicator({
+                visible: false,
+            }).dxLoadIndicator('instance');
+        },
+        onClick(data) {
+            data.component.option('text', 'Sending');
+            buttonIndicator.option('visible', true);
+            setTimeout(() => {
+                buttonIndicator.option('visible', false);
+                data.component.option('text', 'Send');
+            }, 9000);
+        }
+    })
+
+    // Initialize DataGrid with the carts data
+    $("#LoadContainer").dxDataGrid({
+        dataSource: cartsData,
+        columns: [
+            {
+                dataField: "id",
+                caption: "Cart ID"
+            },
+            {
+                dataField: "thumbnail",
+                caption: "Product Thumbnail",
+                cellTemplate: function (container, options) {
+                    console.log("container", container);
+                    console.log("options", options);
+
+                    const thumbnailUrl = options.value;
+                    console.log("thumb nail url", thumbnailUrl)
+                    // Create a container for LoadIndicator and image
+                    const indicatorContainer = $("<div>").appendTo(container);
+                    console.log("indicator container", indicatorContainer)
+
+                    const loadIndicator = $("<div>").dxLoadIndicator({
+                        visible: true,
+                        width: 50,
+                        height: 50
+                    }).appendTo(indicatorContainer);
+                    console.log("loadIndicator", loadIndicator)
+
+                    // Create an image element for the thumbnail
+                    const img = $("<img>").attr("src", thumbnailUrl).css({
+                        width: "100px",
+                        height: "100px",
+                        display: "none"
+                    }).appendTo(indicatorContainer);
+                    console.log("img", img)
+
+                    img.on("load", function () {
+                        loadIndicator.dxLoadIndicator("instance").option("visible", false);
+                        img.css("display", "block");
+                    });
+                }
+            }
+        ],
+
+    });
+});
---
+++ 5-DevExtreme/DevExtreme/Dialogues&Notification/2-loadpanel/panel.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    console.log("doc is ready");
+
+
+    const loadPanel = $("#PanelContainer").dxLoadPanel({
+        message: "Loading data, please wait...", 
+        visible: false,                        
+        showIndicator: true,                    
+        width: 500,                             
+        height: 500,                            
+        position: { my: "center", at: "center", of: window },  
+        animation: { show: { type: "fade",}, hide: { type: "fade",  } } ,
+        //shadingColor:"rgba(255, 99, 71, 0.5)",
+    }).dxLoadPanel("instance");
+
+    // On Button Click
+    $("#loadButton").dxButton({
+        text: "Load Data",
+        onClick: function () {
+            loadPanel.option("visible", true);  
+            setTimeout(function () {
+                //loadPanel.option("visible", false); 
+                loadPanel.hide()
+                dataProductGrid.option("visible",true)
+               //dataProductGrid.show()
+            }, 3000);
+            
+        }
+    });
+
+ const dataProductGrid =   $("#ProductContainer").dxDataGrid({
+        dataSource:products,
+        // starting false after true set in button click
+        visible:false
+    }).dxDataGrid("instance")
+})
